58cd39e2b3c83979c000f2df622b793ff3d81e7a
Yonis7 <yonisaden7@gmail.com>
2025-01-13 15:20:04 -0500

    Add Angular CDK dependency, update app routing, and simplify app component template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// This file defines which pages users can visit in the app and how to get there
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',                                         // When URL has '/home'
+    loadComponent: () => import('./home/home.component'), // Show the home page
+  },
+  {
+    path: '',              // When URL is empty (root path)
+    redirectTo: 'home',    // Send users to the home page
+    pathMatch: 'full',     // Must match URL exactly
+  },
+];
