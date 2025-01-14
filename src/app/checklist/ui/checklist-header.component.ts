933f00e86ca3f39227c5510f25f15caafd438ca2
Yonis7 <yonisaden7@gmail.com>
2025-01-14 14:48:30 -0500

    Add reset button to ChecklistHeaderComponent for removing checklist

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,6 +1,6 @@
 import { Component, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Checklist } from '../../shared/interfaces/checklist';
+import { Checklist, RemoveChecklist } from '../../shared/interfaces/checklist';
 
 @Component({
   selector: 'app-checklist-header',
@@ -11,6 +11,7 @@ import { Checklist } from '../../shared/interfaces/checklist';
         {{ checklist().title }}
       </h1>
       <div>
+        <button (click)="resetChecklist.emit(checklist().id)">Reset</button>
         <button (click)="addItem.emit()">Add item</button>
       </div>
     </header>
@@ -22,4 +23,7 @@ export class ChecklistHeaderComponent {
   checklist = input.required<Checklist>();
   // This component emits the event to add a new item to the checklist when the button is clicked
   addItem = output();
+
+  // This component emits the event to remove the checklist when the button is clicked
+  resetChecklist = output<RemoveChecklist>();
 }
