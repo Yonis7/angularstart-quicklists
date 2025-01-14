1e835d937416d60a9cc88d4bc4001c7c426a8149
Yonis7 <yonisaden7@gmail.com>
2025-01-14 13:46:15 -0500

    Add toggle functionality to ChecklistItemService for managing checklist item states

--- src/app/checklist/data-access/checklist-item.service.ts
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import {
   AddChecklistItem,
   ChecklistItem,
+  RemoveChecklistItem,
 } from '../../shared/interfaces/checklist-item';
 
 export interface ChecklistItemsState {
@@ -26,6 +27,9 @@ export class ChecklistItemService {
   // A stream that we can use to add new checklist items
   add$ = new Subject<AddChecklistItem>();
 
+  // A stream that we can use to delete checklist items
+  toggle$ = new Subject<RemoveChecklistItem>();
+
   constructor() {
     // Listen for new checklist items to be added and update the state
     this.add$.pipe(takeUntilDestroyed()).subscribe((checklistItem) =>
@@ -40,6 +44,18 @@ export class ChecklistItemService {
             checked: false,  // New items start as unchecked
           },
         ],
+      }))
+    );
+
+    // A reducer function to toggle the checked state of a checklist item by ID when it is clicked, and update the state accordingly.
+    this.toggle$.pipe(takeUntilDestroyed()).subscribe((checklistItemId) =>
+      this.state.update((state) => ({
+        ...state,
+        checklistItems: state.checklistItems.map((item) =>
+          item.id === checklistItemId
+            ? { ...item, checked: !item.checked }
+            : item
+        ),
       }))
     );
   }
