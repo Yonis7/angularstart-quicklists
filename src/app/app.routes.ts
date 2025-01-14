5253e0958d4c423f6801fab277f34a65ee6fa472
Yonis7 <yonisaden7@gmail.com>
2025-01-13 23:24:40 -0500

    Add route for checklist page and adjust home redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',                                         // When URL has '/home'
+    path: 'home', // When URL has '/home'
     loadComponent: () => import('./home/home.component'), // Show the home page
   },
   {
-    path: '',              // When URL is empty (root path)
-    redirectTo: 'home',    // Send users to the home page
-    pathMatch: 'full',     // Must match URL exactly
+    path: 'checklist/:id', // When URL has '/checklist/:id'
+    loadComponent: () => import('./checklist/checklist.component'), // Show the checklist page
+  },
+  {
+    path: '', // When URL is empty (root path)
+    redirectTo: 'home', // Send users to the home page
+    pathMatch: 'full', // Must match URL exactly
   },
 ];
